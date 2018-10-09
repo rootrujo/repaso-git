7f6440dbd6f2d196790ac2224d872ba98379f25a
Rodrigo Trujillo <rootrujo@gmail.com>
2018-10-08 21:48:51 -0500

    Tipos de datos y comentarios en Javascript

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+// Forma de comentar en JS
+/*
+ Múltiples líneas 
+*/
+/**
+ * Multimples lineas con astericos que den formato
+ */
+
+// TIPOS DE DATOS 
+
+// Number
+ var numero = 10;
+
+ // String 
+ var string = "Esto es un string";
+ var string2 = 'Esto es un string con comillas simples';
+ var string3 =  `Esto es un string nuevo`;
+
+ // Boolean
+ var boolean1 = true;
+ var boolean2 = false;
+
+ // Null
+ var vacio = null;
+
+ // Undefined
+ var indefinido = undefined;
+ 
