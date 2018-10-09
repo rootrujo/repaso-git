64087885a8dbac0f21c6c96a449c91fbb8524544
Rodrigo Trujillo <rootrujo@gmail.com>
2018-10-08 22:00:52 -0500

    Operadores en JS

--- src/js/app.js
+++ src/js/app.js
@@ -26,3 +26,25 @@
  // Undefined
  var indefinido = undefined;
  
+ /**
+  * OPERADORES ARITMETICOS
+  */
+
+  var suma = 3 + 6;
+  var resta = 3 - 6;
+  var multiplicacion = 3 * 6;
+  var division = 5 / 2;
+
+  // Modulo
+  var modulo = 5 % 2;
+
+  /**
+   * OPERADORES LOGICOS
+   */
+
+   var mayorQue = 9 > 8;
+   var menorQue = 7 < 4;
+   var mayorQueoIgual = 7 >= 7;
+   var menorQueoIgual = 4 <= 3;
+   var igual = 4 == 4;
+   var desigual = 4 != 4;
